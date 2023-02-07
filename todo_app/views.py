afdd8c8118c7bfeb03e451d22fc4c244609c7cd1
HJKLOG <hjklog98@likelion.org>
2023-02-07 18:19:56 +0900

    update func

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import CreateView
+from django.views.generic import CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from . models import Todo
+
+class TodoUpdate(UpdateView):
+    model = Todo
+    fields = ['todo','description','important']
+    template_name = 'todo_app/todo_update_form.html'
 
 # Create your views here.
 class TodoCreate(LoginRequiredMixin ,CreateView):
