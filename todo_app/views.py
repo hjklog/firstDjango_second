23e3a029161cc2d05621970f628a7682d507567f
HJKLOG <hjklog98@likelion.org>
2023-02-07 17:11:49 +0900

    back-up again

--- todo_app/views.py
+++ todo_app/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from . models import Todo
 
 # Create your views here.
-def index(request):
+def Todos(request):
     todos = Todo.objects.all().order_by('-pk')
     return render(
         request,
-        'todo_app/index.html',
+        'todo_app/todos.html',
         {
             'todos':todos,
         }
